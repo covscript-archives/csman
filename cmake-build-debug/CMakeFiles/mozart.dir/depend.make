4136f72fddbe5c0cc007b2aa20bed04d9d940290
Michael Lee <mikecovlee@163.com>
2020-10-28 11:50:45 +0800

    delete garbages

--- cmake-build-debug/CMakeFiles/mozart.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for mozart.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/src/CMakeFiles/csman_src.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcsman_src.a"
-  "libcsman_src.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/csman_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
